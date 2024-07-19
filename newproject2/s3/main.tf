90b1b3e451c77323a6863ee1808270f1b76d80f5
Saravanansanjeevi45 <saravanangambhir@gmail.com>
2024-07-19 17:21:11 +0530

    updated code

--- newproject2/s3/main.tf
+++ newproject2/s3/main.tf
@@ -1,12 +1,13 @@
 provider "aws" {
   region = "us-east-1"
+  access_key = "AKIAZQ3DSU52Y7IEV3X7"
+  secret_key = "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"
 }
 
 resource "aws_s3_bucket" "project" {
   bucket = "project"
   tags = {
-    Name        = "saravanan1"
+    Name        = "saravanan"
     Environment = "Dev"
   }
 }
-
